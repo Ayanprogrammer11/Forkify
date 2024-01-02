d4efa2ca0631b8e3f37861c2ada6ee3199b73a80
Ayanprogrammer11 <ayanliaqat48@gmail.com>
2024-01-02 21:45:15 +0500

    Refactored for the MVC Architecture

---
+++ src/js/model.js
@@ -0,0 +1,27 @@
+import { getJSON } from './helpers.js';
+import { API_URL } from './config.js';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      cookingTime: recipe.cooking_time,
+      image: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+    };
+    console.log(recipe);
+  } catch (err) {
+    console.error(`${err} 💥💥💥💥`);
+  }
+};
